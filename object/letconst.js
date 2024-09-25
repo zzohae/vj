0bbaa752cd055d072d189ee6585fad078d724bfc
zzohae <badahaeyeoncho@gmail.com>
2024-09-25 13:07:26 +0900

    태그 삽입으로 셀렉트형 네비구현하기

--- object/letconst.js
+++ object/letconst.js
@@ -21,13 +21,13 @@ const family = {
 }
 
 let familytag = '';
+familytag += `<h2>${family.d1text}</h2><ul>`;
 
 for ( x of family.familysite ){
   familytag += `<li>${x.atext}</li>`
 }
+familytag +='</ul>';
 
 window.addEventListener('load', function(){
-  this.document.querySelector("#family h2").innerHTML += family.d1text;
-  // 특정 태그 안에 태그를 삽입하기
-  this.document.querySelector("#family ul").innerHTML = familytag;
+  this.document.querySelector("#family").innerHTML = familytag;
 })
